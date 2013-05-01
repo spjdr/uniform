9e7b645119297b9fb3612e69d45049615af60699
Kim G L Pedersen <georglind@gmail.com>
2013-05-01 11:06:29 +0300

    Create Uniform.i18n.php

---
+++ Template/Uniform.i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Internationalization file for skin Uniform.
+ *
+ * @file
+ * @ingroup Skins
+ */
+ 
+$messages = array();
+ 
+/** English + Danish
+ * @author SPJDR
+ */
+$messages['en'] = array(
+        'skinname-uniform' => "Uniform",
+        'uniform-desc' => "SPJDRpedia skin.",
+);
+
+$messages['da'] = array(
+        'skinname-uniform' => "Uniform",
+        'uniform-desc' => "SPJDRpedia skin.",
+);
