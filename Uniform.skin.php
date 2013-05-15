475a56bc0f17093067331486c60be394cb31e353
Sune Amstrup Olsen <suneamstrupolsen@gmail.com>
2013-05-15 17:04:58 +0200

    fejlretning på dropdown-php

--- Uniform.skin.php
+++ Uniform.skin.php
@@ -417,13 +417,13 @@ class UniformTemplate extends BaseTemplate {
 <?php} ?>
 
 		<li id="p-dropdownpil">
-			<ul<?php $this->html( 'userlangattributes' ) ?>>	
+			<?php /* <ul<?php $this->html( 'userlangattributes' ) ?>>	
 			<?php	foreach( $this->getPersonalTools() as $key => $item ) { ?>
 			<?php if($key != "watchlist" && $key != "mycontris" && $key != "userpage"){?>
 			<?php echo $this->makeListItem( $key, $item ); ?>	
 			<?} ?>
 			<?php}  ?> 
-			</ul>
+			</ul> */?>
 		</li>
 
 	</ul>
