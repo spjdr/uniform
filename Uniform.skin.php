c1cf55e595ec408326ad13bc91b0a81382b3ee32
Kim Georg Lind Pedersen <georglind@gmail.com>
2013-05-15 21:26:37 +0200

    Insert search box i sidebar

--- Uniform.skin.php
+++ Uniform.skin.php
@@ -270,6 +270,7 @@ class UniformTemplate extends BaseTemplate {
 			echo "\n<!-- {$name} -->\n";
 			switch( $name ) {
 				case 'SEARCH':
+					$this->renderNavigation('search');
 					break;
 				case 'TOOLBOX':
 					$this->renderPortal( 'tb', $this->getToolbox(), 'toolbox', 'SkinTemplateToolboxEnd' );
