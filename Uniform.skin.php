425d9f07241377115ca46929776744e2d01c498b
Sune Amstrup Olsen <suneamstrupolsen@gmail.com>
2013-05-15 15:40:29 +0200

    test af php $item

--- Uniform.skin.php
+++ Uniform.skin.php
@@ -409,11 +409,20 @@ class UniformTemplate extends BaseTemplate {
 <div id="p-personal" class="<?php if ( count( $this->data['personal_urls'] ) == 0 ) echo ' emptyPortlet'; ?>">
 	<h5><?php $this->msg( 'personaltools' ) ?></h5>
 	<ul<?php $this->html( 'userlangattributes' ) ?>>
-<?php			foreach( $this->getPersonalTools() as $key => $item ) { ?>
-		<?php echo $this->makeListItem( $key, $item ); ?>
-
+<?php	foreach( $this->getPersonalTools() as $key => $item ) { ?>
+		
+		<?php if($item != "Mine bidrag"){?>
+		<?php echo $this->makeListItem( $key, $item ); ?>	
+		<?}?>
 <?php			} ?>
 	</ul>
+
+	<?/*
+	<ul<?php $this->html( 'userlangattributes' ) ?>>
+<?php			foreach( $this->getPersonalTools() as $key => $item ) { ?>
+		<?php echo $this->makeListItem( $key, $item ); ?>
+<?php			} ?>
+	</ul>*/?>
 </div>
 <?php
 				break;
